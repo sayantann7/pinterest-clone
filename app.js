8a8c5fce5ed3e4b6ed2c5140a74dfa472e871e85
Sayantan Nandi <officialsayantannandi@gmail.com>
2024-11-14 14:09:26 +0530

    Done login,signup,profile

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const expressSession = require('express-session');
 const passport = require('passport');
+const flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +15,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(flash());
 
 app.use(expressSession({
   secret: 'your-secret-key',
--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,40 @@ const userModel = require("./users");
 const passport = require("passport");
 const localStrategy = require("passport-local");
 var router = express.Router();
+const upload = require("./multer");
 
-passport.authenticate(new localStrategy(userModel.authenticate()));
+passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index");
+  res.render("signup");
 });
 
-router.get("/profile", isLoggedIn, function (req, res) {
-  res.send("profile");
+router.get("/login",function(req,res){
+  res.render("login",{error:req.flash('error')});
+});
+
+router.get("/profile", isLoggedIn, async function (req, res) {
+  const user = await userModel.findOne({
+    username: req.session.passport.user
+  });
+  res.render("profile",{user});
+});
+
+router.get("/feed",isLoggedIn,async function(req,res){
+  const user = await userModel.findOne({
+    username: req.session.passport.user
+  });
+  res.render('feed',{user});
+});
+
+router.post("/upload",upload.single("file"),function(req,res){
+  if (!req.file){
+    return res.status(400).send("No file uploaded");
+  }else{
+    console.log("File uploaded succesfully");
+    res.redirect("/feed");
+  }
 });
 
 router.post("/register", function (req, res) {
@@ -22,14 +46,15 @@ router.post("/register", function (req, res) {
   userModel.register(userData, req.body.password)
   .then(function () {
     passport.authenticate("local")(req, res, function () {
-      res.redirect("/profile");
+      res.redirect("/feed");
     })
   })
 });
 
 router.post("/login", passport.authenticate("local",{
-  successRedirect: "/profile",
+  successRedirect: "/feed",
   failureRedirect: "/login",
+  failureFlash: true
 }), function (req, res) {});
 
 router.get("/logout",function(req,res){
